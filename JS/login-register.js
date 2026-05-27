6a93d4d39b5198c544c7e93a52bbf5adf1516be3
junisme0705 <115936820+junisme0705@users.noreply.github.com>
2026-05-27 12:07:48 +0700

    update cho Haru Shop

--- JS/login-register.js
+++ JS/login-register.js
@@ -211,4 +211,15 @@ document.addEventListener('DOMContentLoaded', () => {
             // Chuẩn bị link gọi Backend ở bước tiếp theo
         });
     }
+
+    function togglePassword(inputId, icon) {
+        const input = document.getElementById(inputId);
+        if (input.type === 'password') {
+            input.type = 'text';
+            icon.innerHTML = '<i class="fas fa-eye-slash"></i>';
+        } else {
+            input.type = 'password';
+            icon.innerHTML = '<i class="fas fa-eye"></i>';
+        }
+    }
 });
