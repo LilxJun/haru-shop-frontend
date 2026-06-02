9eb612aa49ca27204c36391130ad9b4ab61e73db
junisme0705 <115936820+junisme0705@users.noreply.github.com>
2026-06-02 21:11:12 +0700

    update cho Haru Shop

--- Sever/routes/giveaway-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const giveawayController = require('../controllers/giveaway-controller');
-
-// Đảm bảo tên hàm ở đây khớp với tên hàm đã export bên file controller
-router.get('/check', giveawayController.checkGiveaway);
-router.post('/spin', giveawayController.spinGiveaway);
-
-module.exports = router;
